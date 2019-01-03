bfbb32271b6308d9f29a73bdb0b3be6e12bc0209
midiat <info@midiat.net>
2019-01-03 11:15:47 -0600

    Observables y promesas

---
+++ src/app/pages/rxjs/rxjs.component.ts
@@ -0,0 +1,77 @@
+import { Component, OnInit, RendererFactory2, OnDestroy } from '@angular/core';
+import { Observable, Subscriber, Subscription } from 'rxjs';
+import { retry, map, filter } from 'rxjs/operators';
+
+@Component({
+  selector: 'app-rxjs',
+  templateUrl: './rxjs.component.html',
+  styles: []
+})
+export class RxjsComponent implements OnInit, OnDestroy {
+
+  
+  subscription: Subscription;
+
+  constructor() {
+
+    this.subscription = this.regresaObservable().subscribe(
+      numero =>  console.log('Subs', numero),
+      error => console.error('Error en el obs', error),
+      () => console.log('El observador termino')
+      );
+
+
+  }
+
+  ngOnInit() {
+  }
+
+  ngOnDestroy() {
+    console.log('Destroy');
+    this.subscription.unsubscribe();
+  }
+
+  regresaObservable(): Observable<any> {
+    return new Observable((observer: Subscriber<any>) => {
+      let contador = 0;
+      let intervalo = setInterval(() => {
+
+        contador ++;
+
+        const salida = {
+          valor: contador
+        };
+
+        observer.next(salida);
+
+        // if (contador === 3) {
+        //   clearInterval(intervalo);
+        //   observer.complete();
+        // }
+
+        // if (contador === 2) {
+        //   // clearInterval(intervalo);
+        //   observer.error('SOS');
+        // }
+
+      }, 1000);
+    }).pipe(
+      map( resp =>  resp.valor ),
+      filter((valor, index) => {
+
+        if ((valor % 2) === 1) {
+          // impar
+          return true;
+        } else {
+          // par
+          return false;
+        }
+
+      })
+    );
+
+  }
+
+
+
+}
--- src/app/shared/breadcrumbs/breadcrumbs.component.ts
+++ src/app/shared/breadcrumbs/breadcrumbs.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivationEnd } from '@angular/router';
+import { map, filter } from 'rxjs/operators';
+import { Title, Meta, MetaDefinition } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-breadcrumbs',
@@ -7,9 +10,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BreadcrumbsComponent implements OnInit {
 
-  constructor() { }
+  titulo: string;
+
+  constructor(private router: Router,
+              private titel: Title,
+              private meta: Meta) {
+    this.getDataRoute().subscribe(data => {
+      console.log(data);
+      this.titulo = data.titulo;
+      this.titel.setTitle(this.titulo);
+
+      // Definicion de MetaTags
+      const metaTag: MetaDefinition = {
+        name: 'description',
+        content: this.titulo
+      };
+      this.meta.updateTag(metaTag);
+      //
+
+    });
+  }
 
   ngOnInit() {
+  }
+
+  getDataRoute() {
+    return this.router.events.pipe(
+      filter(evento => evento instanceof ActivationEnd),
+      filter((evento: ActivationEnd) => evento.snapshot.firstChild === null),
+      map((evento: ActivationEnd) => evento.snapshot.data)
+    );
   }
 
 }
