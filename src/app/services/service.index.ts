cfaa771601fd5999fe2307242a3a5d506600337b
midiat <info@midiat.net>
2019-01-20 20:25:20 -0600

    Buscador general, Menu dinamico, Rutas protegidas y Gestion de errores

--- src/app/services/service.index.ts
+++ src/app/services/service.index.ts
@@ -1,12 +1,12 @@
 export { MedicoService } from './medico/medico.service';
 export { HospitalService } from './hospital/hospital.service';
 export { SubirArchivoService } from './uploads/subir-archivo.service';
+export { AdminGuard } from './guards/admin.guard';
 export { LoginGuard } from './guards/login.guard';
 export { UsuarioService } from './usuario/usuario.service';
 export { SharedService } from './shared/shared.service';
 export { SidebarService } from './shared/sidebar.service';
 export { SettingsService } from './settings/settings.service';
-
 
 
 
